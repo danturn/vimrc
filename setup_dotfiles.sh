5ce9a06d379f30e04c5261170e678d804f9870f9
dan <dan@dan-laptop-dev>
2015-10-07 10:50:47 +0100

    getting and installing vundle

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -2,3 +2,9 @@ echo "Backing up .vimrc"
 mv ~/.vimrc ~/.vimrc.old
 echo "Creating symlink to .vimrc"
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then \
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
