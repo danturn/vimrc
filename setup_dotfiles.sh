9179027d35944f4f794b371580b851f40f005c92
dan <danturnhome@googlemail.com>
2020-06-19 13:57:06 +0100

    using vim-plug, adding mix format

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -8,30 +8,8 @@ echo "Removing current vim configuration"
 rm -rf ~/.vim
 rm -f ~/.vimrc
 
-echo 'Installing Vundle - https://github.com/VundleVim/Vundle.vim'
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
-
-echo 'Installing Vim plugins'
-script_dir=`pwd`
-
-echo """
-     set nocompatible
-     filetype off
-     set rtp+=~/.vim/bundle/Vundle.vim
-     call vundle#begin()
-     """  > "${script_dir}"/vimrctemp
-
-grep "Plugin '" .vimrc >> "${script_dir}"/vimrctemp
-echo 'call vundle#end()
-' >> "${script_dir}"/vimrctemp
-
-ln -s "${script_dir}"/vimrctemp ~/.vimrc
-vim +PluginInstall +qall
-
-rm ~/.vimrc
-rm "${script_dir}"/vimrctemp
-
 echo 'Setting up .vimrc'
+script_dir=`pwd`
 ln -s "${script_dir}"/.vimrc ~/.vimrc
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
