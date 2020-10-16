7c9abd9628ab4b4b318a8d8c8b23d3751aaeb8e4
Dan Turner <dan@whitehat.org.uk>
2020-10-16 16:00:21 +0100

    adding vim plugin

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -12,6 +12,9 @@ echo 'Setting up .vimrc'
 script_dir=`pwd`
 ln -s "${script_dir}"/.vimrc ~/.vimrc
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --no-bash
 
